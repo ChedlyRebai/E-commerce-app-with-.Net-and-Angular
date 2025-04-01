41ef0e0490c37228e103cdba3e8cadbc38ae163f
ChedlyRebai <chedly.rebai123@gmail.com>
2025-04-01 15:26:57 +0100

    add: extend AppDbContext to include DbSet properties for Categories, Products, and Photos

--- Infrastructure/Data/AppDbContext.cs
+++ Infrastructure/Data/AppDbContext.cs
@@ -1,10 +1,24 @@
 using System;
+using Core.Entities.Product;
 using Microsoft.EntityFrameworkCore;
 
 namespace Infrastructure.Data;
 
 public class AppDbContext : DbContext
-{   
-    
+{
+    public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+    {
+    }
+
+    public virtual DbSet<Category> Categories { get; set; } 
+    public virtual DbSet<Product> Products { get; set; }
+    public virtual DbSet<Photo> Photos { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+        
+    }
+
 
 }
