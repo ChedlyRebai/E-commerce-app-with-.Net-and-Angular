4129ec07267389a255dfa069c2537dc39190fd0c
ChedlyRebai <chedly.rebai123@gmail.com>
2025-04-03 19:12:11 +0100

    chore: add PhotoConfiguration class to configure Photo entity with initial data

--- Infrastructure/Data/Config/PhotoConfiguration.cs
+++ Infrastructure/Data/Config/PhotoConfiguration.cs
@@ -1,8 +1,19 @@
 using System;
+using Core.Entities.Product;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
 
 namespace Infrastructure.Data.Config;
 
-public class PhotoConfiguration
+public class PhotoConfiguration: IEntityTypeConfiguration<Photo>
 {
+    public void Configure(EntityTypeBuilder<Photo> builder)
+    {
+        builder.HasData(new Photo {
+            Id =1,
+            ProductId = 1,
+            Url = "https://example.com/photo1.jpg",
+        }); 
+    }
 
 }
