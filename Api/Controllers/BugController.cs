14a3a860c14b46e8a88ec6b67e2bcdf5b2d758fd
ChedlyRebai <chedly.rebai123@gmail.com>
2025-04-08 21:38:41 +0100

    feat: Implement API endpoints for handling not found, server error, and bad request scenarios in BugController

--- Api/Controllers/BugController.cs
+++ Api/Controllers/BugController.cs
@@ -1,4 +1,5 @@
 using AutoMapper;
+using Core.DTO;
 using Core.Interfaces;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
@@ -12,5 +13,27 @@ namespace Api.Controllers
         {
             
         }
+
+        [HttpGet("not-found")]
+        public async Task<IActionResult> GetNotFound(){
+            var category = await _unitOfWork.CategoryReppository.GetByIdAsync(42);
+            if(category == null)
+            {
+                return NotFound(new { message = "Category not found" });
+            }
+            return Ok(category);
+        }
+        [HttpGet("server-error")]
+        public async Task<IActionResult> GetServerError(){
+            var category = await _unitOfWork.CategoryReppository.GetByIdAsync(42);
+            var categoryToReturn = _mapper.Map<CategoryDTO>(category);
+            return Ok(categoryToReturn);
+        }
+        [HttpGet("bad-request")]
+        public IActionResult GetBadRequest(int id){
+            return Ok();
+        }
+
+
     }
 }
