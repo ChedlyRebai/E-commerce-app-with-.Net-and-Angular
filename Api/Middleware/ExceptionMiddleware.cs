93d151e1373f4583cefe253d4522e2b90c88be1e
ChedlyRebai <chedly.rebai123@gmail.com>
2025-04-08 23:30:04 +0100

    feat: Refactor ExceptionMiddleware constructor to include IHostEnvironment for improved error handling

--- Api/Middleware/ExceptionMiddleware.cs
+++ Api/Middleware/ExceptionMiddleware.cs
@@ -8,21 +8,26 @@ namespace Api.Middleware;
 
 public class ExceptionMiddleware
 {
+    private readonly IHostEnvironment _environment;
     private readonly RequestDelegate _next;
-    public ExceptionMiddleware(RequestDelegate next){
+    public ExceptionMiddleware(RequestDelegate next,IHostEnvironment environment){
         _next = next;
+        _environment=environment;
     }
 
     public async Task InvokeAsync(HttpContext context){
         try
         {
-            
+            await _next(context);
         }
         catch (Exception ex)
         {
             context.Response.StatusCode=(int)HttpStatusCode.InternalServerError;
             context.Response.ContentType="application/json";
-            var response=new ApiException((int)HttpStatusCode.InternalServerError,ex.Message,ex.StackTrace);
+            
+            var response = _environment.IsDevelopment() ?
+             new ApiException((int)HttpStatusCode.InternalServerError,ex.Message,ex.StackTrace)
+             : new ApiException((int)HttpStatusCode.InternalServerError);
             var json = JsonSerializer.Serialize(response);
             await context.Response.WriteAsync(json);
         }
