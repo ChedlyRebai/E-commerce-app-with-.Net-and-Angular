dd87d21deae435c07f1aca6ad843da8216b373fb
ChedlyRebai <chedly.rebai123@gmail.com>
2025-06-05 11:30:36 +0100

    feat: Add Basket module, component, and service for shopping cart functionality

--- client/src/app/basket/basket.module.ts
+++ client/src/app/basket/basket.module.ts
@@ -2,10 +2,13 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { BasketRoutingModule } from './basket-routing.module';
+import { BasketComponent } from './basket/basket.component';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    BasketComponent
+  ],
   imports: [
     CommonModule,
     BasketRoutingModule
